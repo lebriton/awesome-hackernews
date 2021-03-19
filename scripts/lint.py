5bc0aa322ea87487802244eb47613b6035f1acea
Bryton Lacquement <contact@bminusl.xyz>
2021-03-19 21:53:47 +0100

    lint: rework the linter
    Check for alphabetical order is also added.

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,6 +1,6 @@
-# This script checks the format of the entries in the list.
-
 import re, sys
+from collections import defaultdict
+
 
 _anything_regex = r"(.+)"
 format_1_regex = re.compile(
@@ -8,43 +8,65 @@ format_1_regex = re.compile(
 )
 format_2_regex = re.compile(r"^- \[%s\]\(%s\) - %s\. `%s`$" % ((_anything_regex,) * 4))
 
-lines = []
 
-with open("README.md") as f:
-    # Stupid logic to only process the rows about the list
-    hr_counter = 0
-    for i, line in enumerate(f):
-        if line == "---\n":
-            hr_counter += 1
+def parse_readme():
+    data = defaultdict(list)
+    with open("README.md") as f:
+        # The list is between the 2nd and 3rd horizontal rules (hr).
+        hr_counter = 0
 
-            if hr_counter > 2:
-                break
+        for line_number, line in enumerate(f, start=1):
+            line = line[:-1]
 
-            continue
+            if line == "---":
+                hr_counter += 1
+                if hr_counter > 2:
+                    break
+                continue
+            if hr_counter < 2:
+                continue
 
-        if hr_counter < 2:
-            continue
+            if line.startswith("#"):
+                current_section = (line_number, line.lstrip("#")[1:])
+            elif line.startswith("-"):
+                data[current_section].append((line_number, line))
+    return dict(data)
 
-        if line.startswith("-"):
-            lines.append((i + 1, line[:-1]))
 
-errors = []
-for line_number, line in lines:
-    m = format_1_regex.match(line)
+def lint_entry(entry):
+    line_number, line = entry
+
+    # BBB: walrus operator
+    m = format_1_regex.match(line) or format_2_regex.match(line)
     if m:
-        name, homepage, description, code_url, license = m.groups()
-        continue
+        name = m.group(1)
+        if hasattr(lint_entry, "previous_name"):
+            if name.lower() < lint_entry.previous_name.lower():
+                yield "l%s: entry is not inserted correctly in case-insensitive alphabetical order (compared to l%s)" % (
+                    line_number,
+                    line_number - 1,
+                )
+        # XXX: using this is a bit hacky
+        lint_entry.previous_name = name
+    else:
+        yield "l%s: entry is not formatted correctly" % line_number
 
-    m = format_2_regex.match(line)
-    if m:
-        name, code_url, description, license = m.groups()
-        continue
 
-    errors.append("Line %s is not formatted correctly:\n %s\n" % (line_number, line))
+def lint():
+    errors = []
 
-if errors:
-    for e in errors:
-        print(e)
-    sys.exit(1)
+    for section_name, sublist in parse_readme().items():
+        for entry in sublist:
+            errors.extend(lint_entry(entry))
+        del lint_entry.previous_name
 
-print("All entries appear to be formatted correctly.")
+    if errors:
+        for e in errors:
+            print(e)
+        print("\n%s errors" % len(errors))
+        sys.exit(1)
+    print("0 error")
+
+
+if __name__ == "__main__":
+    lint()
